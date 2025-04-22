cc9621e3df2b4986f6329d720eb5fedcadb20889
Andrew Capaldo <andycapaldo2@gmail.com>
2025-04-21 18:27:13 -0600

    Last commit missed these changes

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 
 app = FastAPI()
 
-drugs = [{"Name": "Atorvastatin", "Brand Name": "Lipitor", "Drug Class": "Statin", "NDC": "0071-0155-23", "Indications": "Hyperlipidemia, prevention of CV events", "Warnings": "May cause liver enzyme abnormalities"}]
+class Drug(BaseModel):
+    Name: str
+    Brand_Name: str
+    Drug_Class: str
+    NDC: str
+    Indications: str
+    Warnings: str
+
+drugs = [{"Name": "Atorvastatin", "Brand_Name": "Lipitor", "Drug_Class": "Statin", "NDC": "0071-0155-23", "Indications": "Hyperlipidemia, prevention of CV events", "Warnings": "May cause liver enzyme abnormalities"}]
 
 app.add_middleware(
     CORSMiddleware,
@@ -17,9 +26,16 @@ def root():
     return {"Hello": "World"}
 
 
-@app.get("/api/drug")
-def get_drug():
+@app.get("/api/drug", response_model=list[Drug])
+def get_drugs():
     if drugs:
         return drugs
-    else:
-        raise HTTPException(status_code=404, detail="Drug not found")
+    raise HTTPException(status_code=404, detail="Drug list is empty")
+
+
+@app.get("/api/drug/{drug_name}", response_model=Drug)
+def get_drug_by_name(drug_name: str):
+    for drug in drugs:
+        if drug['Name'].lower() == drug_name.lower():
+            return drug 
+        raise HTTPException(status_code=404, detail=f"{drug_name} not found")
