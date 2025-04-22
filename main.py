6cbe5514edb4505a3d1ad46cf378d75db28fbfe1
Andrew Capaldo <andycapaldo2@gmail.com>
2025-04-21 18:26:22 -0600

    Added API endpoint, cleaned up requirements folder

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-drugs = [{"name": "Atorvastatin", "brand": "Lipitor", "class": "Statin", "NDC": "0071-0155-23", "indications": "Hyperlipidemia, prevention of CV events", "warnings": "May cause liver enzyme abnormalities"}]
+drugs = [{"Name": "Atorvastatin", "Brand Name": "Lipitor", "Drug Class": "Statin", "NDC": "0071-0155-23", "Indications": "Hyperlipidemia, prevention of CV events", "Warnings": "May cause liver enzyme abnormalities"}]
 
 app.add_middleware(
     CORSMiddleware,
@@ -17,9 +17,9 @@ def root():
     return {"Hello": "World"}
 
 
-@app.get("/drug/{item_id}")
-def get_drug(item_id: int):
-    if item_id < len(drugs):
-        return drugs[item_id]
+@app.get("/api/drug")
+def get_drug():
+    if drugs:
+        return drugs
     else:
         raise HTTPException(status_code=404, detail="Drug not found")
